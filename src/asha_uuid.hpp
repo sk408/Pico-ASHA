f9239d8caa4a7a10d6ae0c3d960181c0e9a2f5d5
sk408 <75577242+sk408@users.noreply.github.com>
2024-10-05 16:53:20 -0700

    update uuid for service

--- src/asha_uuid.hpp
+++ src/asha_uuid.hpp
@@ -40,7 +40,7 @@ namespace AshaUUID
     // 16 bit ASHA service UUID, little endian
     inline constexpr uint16_t service16 = 0xFDF0;
 
-    inline constexpr std::array<uint8_t, 16> service = uuid_from_str("0000FDF0-0000-1000-8000-00805F9B34FB");
+    inline constexpr std::array<uint8_t, 16> service = uuid_from_str("00001800-0000-1000-8000-00805f9b34fb");
     inline constexpr std::array<uint8_t, 16> readOnlyProps = uuid_from_str("6333651e-c481-4a3e-9169-7c902aad37bb");
     inline constexpr std::array<uint8_t, 16> audioControlPoint = uuid_from_str("f0d4de7e-4a88-476c-9d9f-1937b0996cc0");
     inline constexpr std::array<uint8_t, 16> audioStatus = uuid_from_str("38663f1a-e711-4cac-b641-326b56404837");
